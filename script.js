99e515f3398a3eb7e4345d43ea9161f78e7d30cb
Yg-or <inygu@icloud.com>
2021-06-16 18:48:04 -0300

    requisito 19

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const btnLogin = document.getElementById('btnLogin');
+const btnEnviar = document.getElementById('submit-btn');
+const checkBotao = document.getElementById('agreement');
+let botao = true;
 
 function verificaLogin(event) {
   event.preventDefault();
@@ -12,10 +15,17 @@ function verificaLogin(event) {
   }
 }
 
+function liberaBotao() {
+  botao = !botao;
+  btnEnviar.disabled = botao;
+}
+
 function adicionaListener() {
   btnLogin.addEventListener('click', verificaLogin);
+  checkBotao.addEventListener('click', liberaBotao);
 }
 
 window.onload = function load() {
   adicionaListener();
+  btnEnviar.disabled = botao;
 };
